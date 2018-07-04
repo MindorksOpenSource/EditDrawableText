f752533175adb2959296d7cac78db3aed782bb4f
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-07-04 18:15:43 +0530

    Completed project

--- app/src/main/java/com/mindorks/editdrawabletextsample/MainActivity.kt
+++ app/src/main/java/com/mindorks/editdrawabletextsample/MainActivity.kt
@@ -13,15 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val drawable_editText: EditDrawableText = findViewById(R.id.drawable_editText)
-        onClickListeners(drawable_editText)
+        val drawable_editText_left: EditDrawableText = findViewById(R.id.drawable_editText_left)
+        onClickListeners(
+                drawable_editText,
+                drawable_editText_left
+        )
         
     }
     
-    private fun onClickListeners(drawable_editText: EditDrawableText) {
+    private fun onClickListeners(drawable_editText: EditDrawableText, drawable_editText_left: EditDrawableText) {
         drawable_editText.setDrawableClickListener(object : onDrawableClickListener {
             override fun onClick(target: DrawablePosition) {
                 when (target) {
                     DrawablePosition.RIGHT -> displayToastMessage("Clicked Right Drawable").show()
+                }
+            }
+            
+            
+        })
+        drawable_editText_left.setDrawableClickListener(object : onDrawableClickListener {
+            override fun onClick(target: DrawablePosition) {
+                when (target) {
                     DrawablePosition.LEFT -> displayToastMessage("Clicked Left Drawable").show()
                 }
             }

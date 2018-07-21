86c8179abfc4252c28940062f4cbe8f8d8be57a4
pranaypatel512 <www.pranay1337.com@gmail.com>
2018-07-21 23:18:15 +0530

    [+] Added example code for Top and Bottom drawable click

--- app/src/main/java/com/mindorks/editdrawabletextsample/MainActivity.kt
+++ app/src/main/java/com/mindorks/editdrawabletextsample/MainActivity.kt
@@ -1,48 +1,73 @@
 package com.mindorks.editdrawabletextsample
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.mindorks.editdrawabletext.DrawablePosition
-import com.mindorks.editdrawabletext.EditDrawableText
-import com.mindorks.editdrawabletext.onDrawableClickListener
+import com.mindorks.editdrawabletext.OnDrawableClickListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val drawable_editText: EditDrawableText = findViewById(R.id.drawable_editText)
-        val drawable_editText_left: EditDrawableText = findViewById(R.id.drawable_editText_left)
-        onClickListeners(
-                drawable_editText,
-                drawable_editText_left
-        )
-        
+        setOnClickListeners()
     }
     
-    private fun onClickListeners(drawable_editText: EditDrawableText, drawable_editText_left: EditDrawableText) {
-        drawable_editText.setDrawableClickListener(object : onDrawableClickListener {
+    private fun setOnClickListeners() {
+        drawableEditText.setDrawableClickListener(object : OnDrawableClickListener {
             override fun onClick(target: DrawablePosition) {
                 when (target) {
                     DrawablePosition.RIGHT -> displayToastMessage("Clicked Right Drawable").show()
+                    else -> {
+                    }
                 }
             }
             
             
         })
-        drawable_editText_left.setDrawableClickListener(object : onDrawableClickListener {
+        drawableEditTextLeft.setDrawableClickListener(object : OnDrawableClickListener {
             override fun onClick(target: DrawablePosition) {
                 when (target) {
                     DrawablePosition.LEFT -> displayToastMessage("Clicked Left Drawable").show()
+                    else -> {
+                    }
                 }
             }
             
             
         })
+        drawableEditTextTop.setDrawableClickListener(object : OnDrawableClickListener {
+            override fun onClick(target: DrawablePosition) {
+                when (target) {
+                    DrawablePosition.TOP -> displayToastMessage("Clicked TOP Drawable").show()
+                    else -> {
+                    }
+                }
+            }
+
+
+        })
+        drawableEditTextBottom.setDrawableClickListener(object : OnDrawableClickListener {
+            override fun onClick(target: DrawablePosition) {
+                when (target) {
+                    DrawablePosition.BOTTOM -> displayToastMessage("Clicked BOTTOM Drawable").show()
+                    else -> {
+                    }
+                }
+            }
+
+
+        })
     }
-    
-    fun displayToastMessage(message: String): Toast {
-        return Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
-    }
+
+}
+
+/**
+ * Extension function to show Toast in activity
+ */
+fun Activity.displayToastMessage(message: String): Toast {
+    return Toast.makeText(this, message, Toast.LENGTH_SHORT)
 }

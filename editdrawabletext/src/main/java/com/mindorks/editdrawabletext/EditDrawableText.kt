c4ef0e80b1b5e716a045b416dc920c75e8e86d39
Himanshu Singh <hello2himanshusingh@gmail.com>
2019-09-03 01:52:44 +0530

    Feat. Added Option to hide drawable when text is empty

--- editdrawabletext/src/main/java/com/mindorks/editdrawabletext/EditDrawableText.kt
+++ editdrawabletext/src/main/java/com/mindorks/editdrawabletext/EditDrawableText.kt
@@ -1,14 +1,17 @@
 package com.mindorks.editdrawabletext
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.EditText
 import kotlin.math.abs
 
-class EditDrawableText : EditText {
+class EditDrawableText(context: Context, attrs: AttributeSet) : EditText(context, attrs) {
 
     private var drawableRight: Drawable? = null
     private var drawableLeft: Drawable? = null
@@ -16,12 +19,31 @@ class EditDrawableText : EditText {
     private var drawableBottom: Drawable? = null
     private var positionX: Int = 0
     private var positionY: Int = 0
-
+    private var isDrawableShownWhenTextIsEmpty = true
     private var onDrawableClickListener: OnDrawableClickListener? = null
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+    init {
+        parseAttributes(
+                context.obtainStyledAttributes(
+                        attrs,
+                        R.styleable.EditDrawableText
+                )
+        )
+    }
+
+
+    private fun parseAttributes(obtainStyledAttributes: TypedArray) {
+        isDrawableShownWhenTextIsEmpty = obtainStyledAttributes.getBoolean(R.styleable.EditDrawableText_isDrawableShownWhenTextIsEmpty, isDrawableShownWhenTextIsEmpty);
+        obtainStyledAttributes.recycle()
+        hasDrawable(isDrawableShownWhenTextIsEmpty)
+    }
+
+    private fun hasDrawable(value: Boolean) {
+        isDrawableShownWhenTextIsEmpty = value
+        if (!isDrawableShownWhenTextIsEmpty) this.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+        invalidate()
+    }
 
     override fun setCompoundDrawables(leftDrawable: Drawable?,
                                       topDrawable: Drawable?,
@@ -33,99 +55,49 @@ class EditDrawableText : EditText {
         if (bottomDrawable != null) drawableBottom = bottomDrawable
         super.setCompoundDrawables(leftDrawable, topDrawable, rightDrawable, bottomDrawable)
     }
-    
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         var bounds: Rect?
+        val editText = this
+        this.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(char: CharSequence, p1: Int, p2: Int, p3: Int) {
+                if (char.isEmpty()) {
+                    if (!isDrawableShownWhenTextIsEmpty) editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                } else editText.setCompoundDrawables(drawableLeft, drawableTop, drawableRight, drawableBottom)
+            }
+
+
+        })
         if (event.action == MotionEvent.ACTION_DOWN) {
             positionX = event.x.toInt()
             positionY = event.y.toInt()
-           
-            
+
             // this works for left since container shares 0,0 origin with bounds
             if (drawableLeft != null) {
-                bounds = drawableLeft!!.bounds
-
-                var xClickPosition: Int
-                var yClickPosition: Int
-                /*
-                 * @return pixels into dp
-                 */
-                val extraClickArea = (13 * resources.displayMetrics.density + 0.5).toInt()
-
-                xClickPosition = positionX
-                yClickPosition = positionY
-
-                if (!bounds!!.contains(positionX, positionY)) {
-                    /** Gives some extra space for tapping.  */
-                    xClickPosition = positionX - extraClickArea
-                    yClickPosition = positionY - extraClickArea
-
-                    if (xClickPosition <= 0) xClickPosition = positionX
-                    if (yClickPosition <= 0) yClickPosition = positionY
-
-                    /** Creates square from the smallest value  from x or y*/
-                    if (xClickPosition < yClickPosition) yClickPosition = xClickPosition
-                }
-
-                if (bounds.contains(xClickPosition, yClickPosition) && onDrawableClickListener != null) {
-                    onDrawableClickListener!!.onClick(DrawablePosition.LEFT)
-                    event.action = MotionEvent.ACTION_CANCEL
-                    return false
-
-                }
+                bounds = drawableLeft?.bounds
+                setupDrawableLeftClick(bounds, event)
             }
-            
+
             if (drawableRight != null) {
-                bounds = drawableRight!!.bounds
-                var xClickPosition: Int
-                var yClickPosition: Int
-                val extraClickingArea = 13
-                
-                xClickPosition = positionX + extraClickingArea
-                yClickPosition = positionY - extraClickingArea
-                
-                /**
-                 * It right drawable -> subtract the value of x from the width of view. so that width - tapped area                     * will result in x co-ordinate in drawable bound.
-                 */
-                xClickPosition = width - xClickPosition
-                if (xClickPosition <= 0) xClickPosition += extraClickingArea
-                
-                /* If after calculating for extra clickable area is negative.
-                 * assign the original value so that after subtracting
-                 * extra clicking area value doesn't go into negative value.
-                 */
-                
-                if (yClickPosition <= 0) yClickPosition = positionY
-                
-                /**If drawable bounds contains the x and y points then move ahead. */
-                if (bounds!!.contains(xClickPosition, yClickPosition) && onDrawableClickListener != null) {
-                    onDrawableClickListener!!.onClick(DrawablePosition.RIGHT)
-                    event.action = MotionEvent.ACTION_CANCEL
-                    return false
-                }
-                return super.onTouchEvent(event)
+                bounds = drawableRight?.bounds
+                setupDrawableRightClick(bounds, event)
             }
 
             if (drawableTop != null) {
-                bounds = drawableTop!!.bounds
-                val extraClickingArea = 13
-                if (abs((width - paddingLeft - paddingRight) / 2 + paddingLeft - positionX) <= bounds.width() / 2 + extraClickingArea) {
-                    onDrawableClickListener!!.onClick(DrawablePosition.TOP)
-                    event.action = MotionEvent.ACTION_CANCEL
-                    return false
-                }
+                bounds = drawableTop?.bounds
+                setupDrawableTopClick(bounds, event)
             }
 
-            if(drawableBottom!=null)
-            {
-                bounds = drawableBottom!!.bounds
-                val extraClickingArea = 13
-
-                if (abs((width - paddingLeft - paddingRight) / 2 + paddingLeft - positionX) <= bounds.width() / 2 + extraClickingArea) {
-                    onDrawableClickListener!!.onClick(DrawablePosition.BOTTOM)
-                    event.action = MotionEvent.ACTION_CANCEL
-                    return false
-                }
+            if (drawableBottom != null) {
+                bounds = drawableBottom?.bounds
+                setupDrawableBottomClick(bounds, event)
             }
 
 
@@ -133,6 +105,79 @@ class EditDrawableText : EditText {
         return super.onTouchEvent(event)
     }
 
+    private fun setupDrawableBottomClick(bounds: Rect?, event: MotionEvent) {
+        val extraClickingArea = 13
+        if (abs((width - paddingLeft - paddingRight) / 2 + paddingLeft - positionX) <= bounds!!.width() / 2 + extraClickingArea) {
+            onDrawableClickListener?.onClick(DrawablePosition.BOTTOM)
+            event.action = MotionEvent.ACTION_CANCEL
+        }
+    }
+
+    private fun setupDrawableTopClick(bounds: Rect?, event: MotionEvent) {
+        val extraClickingArea = 13
+        if (abs((width - paddingLeft - paddingRight) / 2 + paddingLeft - positionX) <= bounds!!.width() / 2 + extraClickingArea) {
+            onDrawableClickListener?.onClick(DrawablePosition.TOP)
+            event.action = MotionEvent.ACTION_CANCEL
+        }
+    }
+
+    private fun setupDrawableLeftClick(bounds: Rect?, event: MotionEvent) {
+        var xClickPosition: Int
+        var yClickPosition: Int
+        /*
+         * @return pixels into dp
+         */
+        val extraClickArea = (13 * resources.displayMetrics.density + 0.5).toInt()
+
+        xClickPosition = positionX
+        yClickPosition = positionY
+
+        if (!bounds!!.contains(positionX, positionY)) {
+            /** Gives some extra space for tapping.  */
+            xClickPosition = positionX - extraClickArea
+            yClickPosition = positionY - extraClickArea
+
+            if (xClickPosition <= 0) xClickPosition = positionX
+            if (yClickPosition <= 0) yClickPosition = positionY
+
+            /** Creates square from the smallest value  from x or y*/
+            if (xClickPosition < yClickPosition) yClickPosition = xClickPosition
+        }
+
+        if (bounds.contains(xClickPosition, yClickPosition) && onDrawableClickListener != null) {
+            onDrawableClickListener?.onClick(DrawablePosition.LEFT)
+            event.action = MotionEvent.ACTION_CANCEL
+
+        }
+    }
+
+    private fun setupDrawableRightClick(bounds: Rect?, event: MotionEvent) {
+        var xClickPosition: Int
+        var yClickPosition: Int
+        val extraClickingArea = 13
+
+        xClickPosition = positionX + extraClickingArea
+        yClickPosition = positionY - extraClickingArea
+
+        /**
+         * It right drawable -> subtract the value of x from the width of view. so that width - tapped area                     * will result in x co-ordinate in drawable bound.
+         */
+        xClickPosition = width - xClickPosition
+        if (xClickPosition <= 0) xClickPosition += extraClickingArea
+
+        /* If after calculating for extra clickable area is negative.
+         * assign the original value so that after subtracting
+         * extra clicking area value doesn't go into negative value.
+         */
+
+        if (yClickPosition <= 0) yClickPosition = positionY
+
+        /**If drawable bounds contains the x and y points then move ahead. */
+        if (bounds!!.contains(xClickPosition, yClickPosition) && onDrawableClickListener != null) {
+            onDrawableClickListener?.onClick(DrawablePosition.RIGHT)
+            event.action = MotionEvent.ACTION_CANCEL
+        }
+    }
 
     fun setDrawableClickListener(OnDrawableClickListener: OnDrawableClickListener) {
         this.onDrawableClickListener = OnDrawableClickListener
